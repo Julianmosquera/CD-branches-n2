c70b59dadb4b928c55c29cebe38fd6bea521c832
julianlinux <julianlinux@julianlinux-X555LA>
2016-02-22 09:04:56 +0100

    1st commit main

---
+++ src/cd/rama2/CdRama2.java
@@ -0,0 +1,15 @@
+
+package cd.rama2;
+
+/**
+ *
+ * @author julianlinux
+ */
+public class CdRama2 {
+
+    
+    public static void main(String[] args) {
+        //Commit principal para la master 
+    }
+    
+}
